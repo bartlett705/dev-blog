02b515380d3471cd199ad237d83fcc7311dd7e9a
Lance Kanawi <lanceka@gmail.com>
2016-07-01 02:09:57 -0700

    formatted html is prettier with more newlines

--- format_blog.py
+++ format_blog.py
@@ -53,27 +53,27 @@ def format_blog(input_file, output_path):
         if next_post_idx == len(toc):
             next_post_idx = 0
         marked_down = post_header
-        marked_down += '<div class="entry">'
-        marked_down += '\t<h1>' + title + '</h1>'
-        marked_down += '\t<p class="timestamp">[' + timestamp + ']</p>'
-        marked_down += '\t<p class="post-body">' + body + '</p>'
+        marked_down += '<div class="entry">\n'
+        marked_down += '\t<h1>' + title + '</h1>\n'
+        marked_down += '\t<p class="timestamp">[' + timestamp + ']</p>\n'
+        marked_down += '\t<p class="post-body">' + body + '</p>\n'
         marked_down += '</div>'
         marked_down += '\t<div class="tags">' + '\n'
         for tag in tags:
-            marked_down += '\t\t<div class="tag">' + tag + '</div>'
+            marked_down += '\t\t<div class="blog-tag">' + tag + '</div>\n'
     # These next four lines assemble a dictionary of links to pages with each tag
             if tag in tag_dict:
                 tag_dict[tag].append((title, link))
             else:
                 tag_dict[tag] = [(title, link)]
-        marked_down += '\t\n</div>'
-        marked_down += '<br><hr><div class="navbar">'
-        marked_down += '\t&laquo; <a href="'+ toc[previous_post_idx][1] + '">' + toc[previous_post_idx][0] + '</a> | <a href="index.html">Contents</a> |<a href="' + toc[next_post_idx][1] + '">' + toc[next_post_idx][0] + '</a> &raquo;\t'
-        marked_down += '</div>'                                                                                                                                                     
+        marked_down += '\t\n</div>\n'
+        marked_down += '<br><hr><div class="navbar">\n'
+        marked_down += '\t&laquo; <a href="'+ toc[previous_post_idx][1] + '">' + toc[previous_post_idx][0] + '</a> | <a href="index.html">Contents</a> |<a href="' + toc[next_post_idx][1] + '">' + toc[next_post_idx][0] + '</a> &raquo;\n'
+        marked_down += '</div>\n'                                                                                                                                                     
         marked_down += post_footer
 
         index_markdown += '<li class="toc_entry">[' + timestamp + '] :\
-        <a href="' + link + '"><strong>' + title + '</strong></a></li>'
+        <a href="' + link + '"><strong>' + title + '</strong></a></li>\n'
         of = file(output_path + link, 'w')
         of.write(marked_down)
         of.close()
@@ -83,13 +83,13 @@ def format_blog(input_file, output_path):
 
     print 'Generating index in ' + output_path + '...'
 
-    index_markdown += '<h2>Tags</h2><div class="tag-list"><ul>'
+    index_markdown += '<h2>Tags</h2>\n<div class="tag-list">\n<ul>\n'
     for tag, post_list in tag_dict:
         index_markdown += '<li class="tag-name">'+ tag + ' <div class="tagged-posts">: '
         for title, link in post_list :
             index_markdown += '<a href="' + link + '">' + title + '</a>, '
         index_markdown = index_markdown[:-2]
-    index_markdown += '</div></ul></div>'
+    index_markdown += '\n</div>\n</ul>\n</div>\n'
     index_markdown += index_footer
     index_file = file(output_path + 'index.html', 'w')
     index_file.write(index_markdown)
